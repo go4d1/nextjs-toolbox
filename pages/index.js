1113930c76934c395dcc5bcb4d7f55ec0fde8053
Martin Glynn <oline@bestweb.net>
2024-07-30 17:14:04 -0400

    Looking Good

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox is Trying to commit?" />
+        <Header title="Next.js Toolbox - Looking Good" />
         <hr />
         <p className="description">
           Here's an example of a Netlify Form! When you fill this out, the
