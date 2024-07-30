96a45d277fc76cd73bab05a2e232aaf3ca7438cf
Martin Glynn <oline@bestweb.net>
2024-07-30 15:02:53 -0400

    what

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox is 80 years old to old to be writing code?" />
+        <Header title="Next.js Toolbox is Trying to commit?" />
         <hr />
         <p className="description">
           Here's an example of a Netlify Form! When you fill this out, the
